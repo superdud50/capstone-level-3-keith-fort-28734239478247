42069aa2e0adbe0259ed18277fb9cefb78ad5dd0
superdud50 <crissxcringlesxdead@gmail.com>
2024-10-22 23:11:13 -0500

    myNewAppointment

--- utility-functions.js
+++ utility-functions.js
@@ -16,13 +16,29 @@ function createContainer(rowString){
 }
 
 function myProcessForm(event){
-  debugger
+  
 event.preventDefault();
 const form = event.target;
 const firstNameInput = form[0];
 const value = firstNameInput.value;
-output(`Your infomation has been processed, your email is ${value}.<br>`);
+output(`Your infomation has been processed, your email is ${value}.<br>`)
 }
+
+function myAppointmentForm(event){
+  
+event.preventDefault();
+const form = event.target;
+const firstNameInput = form[0];
+const lastNameInput = form [1];
+const phoneNumberInput= form[2];
+const dateTimeInput= form[3]
+const value1 = firstNameInput.value;
+const value2 = lastNameInput.value;
+const value3 = phoneNumberInput.value;
+const value4 = dateTimeInput.value;
+output(`Hello ${value1 +" "+ value2 }.We have received your order, your conact has been saved as ${value3} and your appointment has been scheduled for ${value4} <br>`)
+}
+
 function output(text){
   document.write(text)
 }
