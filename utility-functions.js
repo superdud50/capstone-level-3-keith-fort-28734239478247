b01a7b6ad52b173dbd79081c0b31d9581e549e29
superdud50 <crissxcringlesxdead@gmail.com>
2024-10-22 22:49:21 -0500

    myAppointment

--- utility-functions.js
+++ utility-functions.js
@@ -14,3 +14,15 @@ function createContainer(rowString){
     </div>`
   return containerString;
 }
+
+function myProcessForm(event){
+  debugger
+event.preventDefault();
+const form = event.target;
+const firstNameInput = form[0];
+const value = firstNameInput.value;
+output(`Your infomation has been processed, your email is ${value}.<br>`);
+}
+function output(text){
+  document.write(text)
+}
